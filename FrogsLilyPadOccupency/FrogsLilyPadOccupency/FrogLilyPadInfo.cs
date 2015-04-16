f893c90bd9d2472cc118c7acf89a30f50652765f
mohtshm <testmohtshm@gmail.com>
2015-04-17 00:37:08 +0500

    technical test
    For Unified logic test

---
+++ FrogsLilyPadOccupency/FrogsLilyPadOccupency/FrogLilyPadInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrogsLilyPadOccupency
+{
+    class FrogLilyPadInfo
+    {
+        public string Pond { get; set; }
+
+        public string Frog { get; set; }
+
+        public string ResidingPad { get; set; }
+
+        public DateTime PresenceTime { get; set; }
+
+        public bool IsLeaving { get; set; }
+    }
+}
