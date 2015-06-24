e988922ae88110ccb4e5d3ce7160a2fa93128005
mohtshm <testmohtshm@gmail.com>
2015-06-24 15:08:12 +0500

    webperformance
    for data to create web perfomance test

--- Dotnet/DBchanges Data migration/Stats.sql
+++ Dotnet/DBchanges Data migration/Stats.sql
@@ -1,3 +1,4 @@
+
 --select * from AspNetUserClaims
 --select * from AspNetRoles
 --select * from AspNetUserRoles
@@ -7,6 +8,7 @@
 -- 700 91434 91794  92516 -- 7128   24815-25720
 -- 257 120458  312 124700
 -- 1258 36883 1259 37629 100 38401 142 64416
+-- 300 > 416 5671 417 7485 425 17787 443 32336 516 
 select count(id) from AspNetUsers with (nolock)
 --select  count(id) from order_details 	
 
