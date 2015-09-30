3a3731f1b728ce818e42ed8ca66fb511971840c8
mohtshm <testmohtshm@gmail.com>
2015-09-30 19:36:35 +0500

    updated

--- FinalScriptOrder.sql
+++ FinalScriptOrder.sql
@@ -1,1 +1,17 @@
 mysql> SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS;SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION;SET NAMES utf8;SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0;CALL add_product_entity_decimal(); CALL add_product_entity_datetime(); CALL add_product_entity_int(); CALL  `add_product_url_rewrite`();CALL `add_product_index_price`();SET SQL_MODE=@OLD_SQL_MODE;SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT;SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS;SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION;SET SQL_NOTES=@OLD_SQL_NOTES;
+
+
+
+
+* * * * * wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 5; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 10; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 15; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 20; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 25; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 30; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 35; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 40; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 45; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 50; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
+* * * * * sleep 55; wget http://staging.crazydeals.com/excel_controller/order_export_zoho
